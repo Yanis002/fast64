699c79992dec9e76e35b782f6d96bd0c75fb40bc
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2022-10-01 14:04:13 +0200

    spec

--- fast64_internal/oot/c_writer/oot_spec.py
+++ fast64_internal/oot/c_writer/oot_spec.py
@@ -1,117 +1,130 @@
-import os, re
-from ...utility import *
-from ..oot_utility import *
+import os, re, bpy
+from ...utility import readFile, writeFile
+from ..oot_utility import getSceneDirFromLevelName, indent, ExportInfo
+from ..oot_level_classes import OOTScene
+from ..c_writer.oot_level_c import OOTLevelC
 
-def getSegmentDefinitionEntryBySceneName(segmentDefinition, sceneName):
-	entries = []
-	for entry in segmentDefinition:
-		matchText = "\s*name\s*\"" + sceneName + "\_"
-		if re.match(matchText + "scene\"", entry) or re.match(matchText + "room\_\d+\"", entry):
-			entries.append(entry)
-	return entries
 
-def readSegmentDefinition(exportPath):
-	fileData = readFile(os.path.join(exportPath, 'spec'))
-	segmentDefinition, compressFlag = parseSegmentDefinitionData(fileData)
+def getSegmentDefinitionEntryBySceneName(segmentDefinition: list[str], sceneName: str):
+    entries = []
 
-	return segmentDefinition, fileData, compressFlag
+    for entry in segmentDefinition:
+        matchText = '\s*name\s*"' + sceneName + "\_"
+        if re.match(matchText + 'scene"', entry) or re.match(matchText + 'room\_\d+"', entry):
+            entries.append(entry)
 
-def parseSegmentDefinitionData(data):
-	table = []
-	compressFlag = ""
-	for match in re.finditer('beginseg(((?!endseg).)*)endseg', data, re.DOTALL):
-		segData = match.group(1)
-		table.append(segData)
+    return entries
 
-		# avoid deleting compress flag if the user is using it
-		# (defined by whether it is present at least once in spec or not)
-		if "compress" in segData:
-			compressFlag = indent + "compress\n"
 
-	return table, compressFlag
+def parseSegmentDefinitionData(fileData: str):
+    segmentData: list[str] = []
+    compressFlag = ""
+    for match in re.finditer("beginseg(((?!endseg).)*)endseg", fileData, re.DOTALL):
+        segData = f"{match.group(1)}"
+        segmentData.append(segData)
 
-def segmentDefinitionToString(segmentDefinitions):
-	data = '/*\n * ROM spec file\n */\n\n'
-	for entry in segmentDefinitions:
-		data += "beginseg" + entry + "endseg\n\n"
+        # avoid deleting compress flag if the user is using it
+        # (defined by whether it is present at least once in spec or not)
+        if "compress" in segData:
+            compressFlag = indent + "compress\n"
 
-	# return the data and remove the extra ``\n`` at the end of the file
-	return data[:-1]
+    return segmentData, compressFlag
 
-def writeSegmentDefinition(segmentDefinition, fileData, exportPath):
-	newFileData = segmentDefinitionToString(segmentDefinition)
 
-	if newFileData != fileData:
-		writeFile(os.path.join(exportPath, 'spec'), newFileData)
+def writeSegmentDefinition(segmentDefinition: list[str], fileData: str, exportPath: str):
+    newFileData = "/*\n * ROM spec file\n */\n\n" + "\n\n".join(
+        ["beginseg" + entry + "endseg" for entry in segmentDefinition]
+    )
 
-def modifySegmentDefinition(scene, exportInfo, levelC):
-	exportPath = exportInfo.exportPath
-	segmentDefinitions, fileData, compressFlag = readSegmentDefinition(exportPath)
-	sceneName = scene.name if scene is not None else exportInfo.name
-	entries = getSegmentDefinitionEntryBySceneName(segmentDefinitions, sceneName)
+    if newFileData != fileData:
+        writeFile(os.path.join(exportPath, "spec"), newFileData)
 
-	if exportInfo.customSubPath is not None:
-		includeDir = 'build/' + exportInfo.customSubPath + sceneName + '/' + sceneName
-	else:
-		includeDir = 'build/' + getSceneDirFromLevelName(sceneName) + '/' + sceneName
 
-	if len(entries) > 0:
-		firstIndex = segmentDefinitions.index(entries[0])
-		for entry in entries:
-			segmentDefinitions.remove(entry)
-	else:
-		firstIndex = len(segmentDefinitions)
+def modifySegmentDefinition(scene: OOTScene, exportInfo: ExportInfo, levelC: OOTLevelC):
+    exportPath = exportInfo.exportPath
 
-	if scene is not None:
-		if bpy.context.scene.ootSceneSingleFile:
-			segmentDefinitions.insert(firstIndex,
-				'\n' + indent + 'name "' + scene.name + '_scene"\n' +\
-				compressFlag +\
-				indent + "romalign 0x1000\n" +\
-				indent + 'include "' + includeDir + '_scene.o"\n' +\
-				indent + "number 2\n")
-			firstIndex += 1
+    # read spec file data
+    fileData = readFile(os.path.join(exportPath, "spec"))
+    segmentDefinitions, compressFlag = parseSegmentDefinitionData(fileData)
 
-			for i in range(len(scene.rooms)):
-				roomSuffix = "_room_" + str(i)
-				segmentDefinitions.insert(firstIndex, 
-					'\n' + indent + 'name "' + scene.name + roomSuffix + '"\n' +\
-					compressFlag +\
-					indent + "romalign 0x1000\n" +\
-					indent + 'include "' + includeDir + roomSuffix + '.o"\n' +\
-					indent + "number 3\n")
-				firstIndex += 1
-		else:
-			sceneSegInclude = '\n' + indent + 'name "' + scene.name + '_scene"\n' +\
-				compressFlag +\
-				indent + "romalign 0x1000\n" +\
-				indent + 'include "' + includeDir + '_scene_main.o"\n' +\
-				indent + 'include "' + includeDir + '_scene_col.o"\n'
-			
-			if levelC is not None:
-				if (levelC.sceneTexturesIsUsed()):
-					sceneSegInclude += indent + 'include "' + includeDir + '_scene_tex.o"\n'
-				
-				if (levelC.sceneCutscenesIsUsed()):
-					for i in range(len(levelC.sceneCutscenesC)):
-						sceneSegInclude += indent + 'include "' + includeDir + '_cs_' + str(i) + '.o"\n'
-				
-			sceneSegInclude += indent + "number 2\n"
+    sceneName = scene.name if scene is not None else exportInfo.name
+    entries = getSegmentDefinitionEntryBySceneName(segmentDefinitions, sceneName)
 
-			segmentDefinitions.insert(firstIndex, sceneSegInclude)
+    if exportInfo.customSubPath is not None:
+        scenePath = exportInfo.customSubPath + sceneName
+    else:
+        scenePath = getSceneDirFromLevelName(sceneName)
+    includeDir = f"build/{scenePath}/{sceneName}"
 
-			firstIndex += 1
+    if len(entries) > 0:
+        firstIndex = segmentDefinitions.index(entries[0])
+        for entry in entries:
+            segmentDefinitions.remove(entry)
+    else:
+        firstIndex = len(segmentDefinitions)
 
-			for i in range(len(scene.rooms)):
-				roomSuffix = "_room_" + str(i)
-				segmentDefinitions.insert(firstIndex, 
-					'\n' + indent + 'name "' + scene.name + roomSuffix + '"\n' +\
-					compressFlag +\
-					indent + "romalign 0x1000\n" +\
-					indent + 'include "' + includeDir + roomSuffix + '_main.o"\n' +\
-					indent + 'include "' + includeDir + roomSuffix + '_model_info.o"\n' +\
-					indent + 'include "' + includeDir + roomSuffix + '_model.o"\n' +\
-					indent + "number 3\n")
-				firstIndex += 1
+    if scene is not None:
+        if bpy.context.scene.ootSceneSingleFile:
+            segmentDefinitions.insert(
+                firstIndex,
+                "\n"
+                + (indent + f'name "{scene.sceneName()}"\n')
+                + compressFlag
+                + (indent + "romalign 0x1000\n")
+                + (indent + f'include "{includeDir}_scene.o"\n')
+                + (indent + "number 2\n"),
+            )
+            firstIndex += 1
 
-	writeSegmentDefinition(segmentDefinitions, fileData, exportPath)
+            for i in range(len(scene.rooms)):
+                roomSuffix = f"_room_{i}"
+                segmentDefinitions.insert(
+                    firstIndex,
+                    "\n"
+                    + (indent + f'name "{scene.name + roomSuffix}"\n')
+                    + compressFlag
+                    + (indent + "romalign 0x1000\n")
+                    + (indent + f'include "{includeDir + roomSuffix}".o\n')
+                    + (indent + "number 3\n"),
+                )
+                firstIndex += 1
+        else:
+            sceneSegInclude = (
+                "\n"
+                + (indent + f'name "{scene.sceneName()}"\n')
+                + compressFlag
+                + (indent + "romalign 0x1000\n")
+                + (indent + f'include "{includeDir}_scene_main.o"\n')
+                + (indent + f'include "{includeDir}_scene_col.o"\n')
+            )
+
+            if levelC is not None:
+                if levelC.sceneTexturesIsUsed():
+                    sceneSegInclude += indent + f'include "{includeDir}_scene_tex.o"\n'
+
+                if levelC.sceneCutscenesIsUsed():
+                    sceneSegInclude += "".join(
+                        [indent + f'include "{includeDir}_cs_{i}.o"\n' for i in range(len(levelC.sceneCutscenesC))]
+                    )
+
+            sceneSegInclude += indent + "number 2\n"
+            segmentDefinitions.insert(firstIndex, sceneSegInclude)
+            firstIndex += 1
+
+            for i in range(len(scene.rooms)):
+                roomSuffix = "_room_" + str(i)
+                roomDir = includeDir + roomSuffix
+                segmentDefinitions.insert(
+                    firstIndex,
+                    "\n"
+                    + (indent + f'name "{scene.name + roomSuffix}"\n')
+                    + compressFlag
+                    + (indent + "romalign 0x1000\n")
+                    + (indent + f'include "{roomDir}_main.o"\n')
+                    + (indent + f'include "{roomDir}_model_info.o"\n')
+                    + (indent + f'include "{roomDir}_model.o"\n')
+                    + (indent + "number 3\n"),
+                )
+                firstIndex += 1
+
+    writeSegmentDefinition(segmentDefinitions, fileData, exportPath)
