a80a9b64b454f232b0e6a130df1922315abff819
Lila <lilaa3public@gmail.com>
2026-02-02 17:33:32 +0000

    [OOT] clean up getOoTRotation and remove broken assert (#658)

--- fast64_internal/utility.py
+++ fast64_internal/utility.py
@@ -646,7 +646,10 @@ def cast_integer(value: int, bits: int, signed: bool):
 
 
 to_s16 = lambda x: cast_integer(round(x), 16, True)
-radians_to_s16 = lambda d: to_s16(d * 0x10000 / (2 * math.pi))
+
+
+def radians_to_s16(value: float, signed=True) -> int:
+    return cast_integer(round(value * 2**16 / (2 * math.pi)), 16, signed)
 
 
 def int_from_s16(value: int) -> int:
